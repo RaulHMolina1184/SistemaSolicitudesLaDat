6c207368e8f0d601c650ab3fcb73b23014c18361
Raul Hernandez Molina <112280077@cuc.cr>
2025-06-13 20:16:34 -0600

    Funcionalidad módulo de Gestión de Usuarios funcional. Elementos visuales terminados y ajustados. Por agregar funcionalidad del Módulo Bitacora.

---
+++ SistemaSolicitudesLaDat.Entities/Bitacora/Bitacora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaSolicitudesLaDat.Entities.Bitacora
+{
+    public class Bitacora
+    {
+        public string IdBitacora { get; set; }  // CHAR(10)
+        public DateTime Fecha { get; set; }  // DATE
+        public string UsuarioEjecutor { get; set; }  // CHAR(10)
+        public string DescripcionAccion { get; set; }  // VARCHAR(100)
+        public string ListadoAcciones { get; set; }  // JSON almacenado como string
+        public string IdSolicitud { get; set; }  // CHAR(10)
+    }
+
+}
